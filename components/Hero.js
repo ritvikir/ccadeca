5685635a5bd642be6dabd4685d5503785febd8d5
ritvikir <ririgireddy1@gmail.com>
2023-08-06 17:27:29 -0700

    finish hero mvp

---
+++ components/Hero.js
@@ -0,0 +1,16 @@
+export default function Hero() {
+    return (
+      <div className="h-screen flex flex-col">
+        <div className="h-3/4 bg-[url('https://assets-global.website-files.com/600faaecb03ce57f6fa98050/616f727efd1a14625e6a9aac_DECA_042817-63.jpg')] bg-cover bg-center">
+          <div className="h-full flex justify-center items-center bg-blue-600/30 backdrop-brightness-75">
+            <span className="text-white font-bold text-6xl">
+              Canyon Crest Academy DECA
+            </span>
+
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar"
-
+import Hero from "@/components/Hero";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,6 +9,11 @@ export default function Home() {
   return (
     <main>
       <Navbar/>
+      <Hero/>
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <h1>aslklaks</h1>
+      </div>
+      
     </main>
   );
 }
