eab7b42ea2e8d55d85a9eb620754f1f8ee5b1577
ritvikir <ririgireddy1@gmail.com>
2023-11-18 19:58:23 -0800

    add meta

--- pages/index.js
+++ pages/index.js
@@ -16,15 +16,32 @@ export default function Home() {
     <main>
       <Head>
         <title>Canyon Crest Academy DECA</title>
+        <meta
+          name="description"
+          content="Join DECA to explore the realms of marketing, finance, hospitality, and management while developing leadership and entrepreneurial skills."
+          key="desc"
+        />
+        <meta
+          property="og:title"
+          content="DECA: Empowering Emerging Leaders and Entrepreneurs"
+        />
+        <meta
+          property="og:description"
+          content="DECA prepares the next generation of leaders and innovators, offering students unique opportunities to grow through competitions, conferences, and networking."
+        />
+        <meta
+          property="og:image"
+          content="https://wadeca.org/wp-content/uploads/2019/09/DECA-Diamond-1.png"
+        />
       </Head>
 
       <Navbar />
       <Hero />
       <About />
-      <Calendar/>
-      <Team/>
-      <Faq/>
-      <Footer/>
+      <Calendar />
+      <Team />
+      <Faq />
+      <Footer />
     </main>
   );
 }
